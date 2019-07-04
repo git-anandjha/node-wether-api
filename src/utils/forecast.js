8a0d54065776834afae71de90b12dd65b675c59e
clumsycoder <tosendanand@gmail.com>
2019-07-04 18:33:06 +0530

    last commit

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,8 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+    const url=  'https://api.darksky.net/forecast/57a5a0fe5beee521ff0781ec02b19a76/'+latitude+','+longitude+'?units=si'
+    // const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
