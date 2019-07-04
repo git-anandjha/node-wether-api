f4e18b4aa3d2a73deedfc7720e073dbe07415024
clumsycoder <tosendanand@gmail.com>
2019-07-04 18:41:26 +0530

    something was changed

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const forecast = require('./utils/forecast')
 
 const app = express()
 
-// Define paths for Express config
+// Define paths for Express configuration
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
